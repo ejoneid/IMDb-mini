c30c1572fa92e0d06b0cae7c262d553f2ec9fb6a
Eirik Joneid <eirijomine@hotmail.com>
2021-09-24 14:14:50 +0200

    init

---
+++ src/main/java/no/noroff/moviecharacters/MoviecharactersApplication.java
@@ -0,0 +1,13 @@
+package no.noroff.moviecharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecharactersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/noroff/moviecharacters/MoviecharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package no.noroff.moviecharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
